3a9f7a9f2fa35bf9f7e7bacc25a9eacf3e664871
HunYul <129160556+HunYul@users.noreply.github.com>
2025-04-19 16:42:36 +0900

    0419

--- Battle/Battle/Battle.cpp
+++ Battle/Battle/Battle.cpp
@@ -14,6 +14,38 @@
 // extra)Player는 3가지 직업 중 하나를 선택
 // Knight와 고블린이 서로 죽을 때까지 싸움
 
+// SoundManager) 4/19 추가
+// 플레이어 공격 및 피격 시 메세지 출력.
+
+
+class SoundManager
+{
+private:
+    SoundManager() {}
+    ~SoundManager() {}
+
+    static SoundManager* instance;
+
+public:
+    static SoundManager* GetInstance()
+    {
+        if (instance == nullptr) {
+            instance = new SoundManager();
+        }
+        return instance;
+
+    }
+
+    void PlayerAttack() {
+        cout << "플레이어가 공격 했습니다." << endl;
+    }
+
+    void PlayerHit() {
+        cout << "고블린이 공격 했습니다." << endl;
+    }
+
+};
+
 class Battle
 {
 public:
@@ -24,8 +56,11 @@ public:
     void StartFight()
     {
         while (_knight->IsAlive() && _goblin->IsAlive()) {
+            SoundManager::GetInstance()->PlayerAttack();
             _knight->Attack(_goblin);
+
             if (_goblin->IsAlive()) {
+                SoundManager::GetInstance()->PlayerHit();
                 cout << "고블린이 플레이어에게 " << _goblin->GetATK() << "의 만큼의 피해를 입혔습니다." << endl;
                 _knight->TakeDamage(_goblin->GetATK(), "플레이어의 ");
             }
@@ -45,6 +80,9 @@ private:
     Knight* _knight;
     Goblin* _goblin;
 };
+
+
+SoundManager* SoundManager::instance = nullptr;
 
 int main()
 {
--- Battle/Battle/Knight.cpp
+++ Battle/Battle/Knight.cpp
@@ -7,3 +7,6 @@ void Knight::Attack(Goblin* goblin)
     cout << "플레이어가 고블린에게 " << _atk << "만큼의 피해를 입혔습니다." << endl;
     goblin->TakeDamage(_atk, "고블린의 ");  // Goblin에게 피해를 입히고, "Goblin"을 이름으로 전달
 }
+
+
+// Sound Manager로 이전
